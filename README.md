96f398cae0177412fa41608d555b755a9cfbd897
Dize dev <axiozweb2022@gmail.com>
2024-05-04 17:02:58 -0500

    proyecto terminado

---
+++ README.md
@@ -0,0 +1,54 @@
+# Editor de Imágenes JS
+
+Este proyecto es un editor de imágenes basado en la web que permite a los usuarios aplicar filtros y transformaciones a sus imágenes de manera interactiva.
+
+## Características
+
+- Carga de imágenes desde el dispositivo del usuario.
+- Aplicación de filtros como brillo, saturación, inversión y escala de grises.
+- Transformaciones de imagen como rotar y voltear.
+- Vista previa en tiempo real de las ediciones.
+- Funcionalidad para restablecer los filtros y transformaciones a los valores por defecto.
+- Opción para descargar la imagen editada.
+
+## Tecnologías Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Cómo Usar
+
+1. Clona el repositorio en tu máquina local.
+2. Abre el archivo `index.html` en tu navegador para iniciar la aplicación.
+3. Utiliza los botones y deslizadores proporcionados en la interfaz para editar tu imagen.
+4. Guarda la imagen editada utilizando el botón 'Guardar imagen'.
+
+## Contribuir
+
+Las contribuciones son lo que hacen que la comunidad de código abierto sea un lugar increíble para aprender, inspirar y crear. Cualquier contribución que hagas será **muy apreciada**.
+
+1. Haz Fork del proyecto.
+2. Crea tu rama de características (`git checkout -b feature/AmazingFeature`).
+3. Haz commit de tus cambios (`git commit -m 'Add some AmazingFeature'`).
+4. Haz Push a la rama (`git push origin feature/AmazingFeature`).
+5. Abre una Pull Request.
+
+![Logo de mi proyecto](https://raw.githubusercontent.com/usuario/repositorio/rama/ruta/al/asset)
+ 
+ 
+## Licencia
+
+Distribuido bajo la Licencia MIT. Consulta `LICENSE` para más información.
+
+## Contacto
+
+Tu Nombre - @tu_twitter - email@example.com
+
+Link del Proyecto: https://github.com/tu_usuario/tu_repositorio
+
+## Agradecimientos
+
+- Choose an Open Source License
+- Font Awesome
+- GitHub Pages
---
+++ js/app.js
@@ -0,0 +1,183 @@
+
+const fileInput = document.querySelector('.file-input'),
+    previeImg = document.querySelector('.previw-image img'),
+    filterOptions = document.querySelectorAll('.filter button'),
+    rotateOptions = document.querySelectorAll('.rotate button'),
+    filterName = document.querySelector('.slider .name'),
+    filterSlider = document.querySelector('.slider input'),
+    filterValue = document.querySelector('.filter-info .value'),
+    restFilterBtn = document.querySelector('.rest-filters'),
+    choosoBtn = document.querySelector('.choose-img'),
+    saveImageBtn = document.querySelector('.Seve-image');
+
+let Brightness = 100,
+    Saturation = 100,
+    Inversion = 0,
+    GrayScale = 0;
+
+let rotate = 0,
+    flipHorizontal = 1,
+    flipVertical = 1;
+
+const filtros = {
+    BRIGHTNESS: 'brightness',
+    SATURATION: 'Saturation',
+    INVERSION: 'Inversion',
+    GRAYSCALE: 'GrayScale'
+}
+
+const direccion = {
+    LEFT: 'left',
+    RIGHT: 'right',
+    HORIZONTAL: 'horizontal',
+    VERTICAL: 'vertical'
+}
+
+
+const appyFilter = () => {
+
+    const editFilter = {
+        ROTATE: `rotate(${rotate}deg) scale(${flipHorizontal}, ${flipVertical})`,
+        FILTER: `brightness(${Brightness}%) saturate(${Saturation}%) invert(${Inversion}%) grayscale(${GrayScale})`
+    };
+
+    previeImg.style.transform = editFilter.ROTATE;
+    previeImg.style.filter = editFilter.FILTER;
+};
+
+const loadImagen = () => {
+
+    let file = fileInput.files[0];
+    if (!file) return;
+    previeImg.src = URL.createObjectURL(file);
+
+    previeImg.addEventListener('load', () => {
+        document.querySelector('.container').classList.remove('disable');
+    });
+    console.log(file);
+};
+
+
+[...filterOptions].map(option => {
+    option.addEventListener('click', () => {
+
+        document.querySelector('.filter .active').classList.remove('active');
+
+        option.classList.add('active');
+        filterName.textContent = option.textContent;
+
+        if (option.id === filtros.BRIGHTNESS) {
+
+            filterSlider.max = 200;
+            filterSlider.value = Brightness;
+            filterValue.textContent = `${Brightness} %`;
+
+        } else if (option.id === filtros.SATURATION) {
+
+            filterSlider.max = 200;
+            filterSlider.value = Saturation;
+            filterValue.textContent = `${Saturation} %`;
+
+        } else if (option.id === filtros.INVERSION) {
+
+            filterSlider.max = 100;
+            filterSlider.value = Inversion;
+            filterValue.textContent = `${Inversion} %`;
+
+        } else {
+
+            filterSlider.max = 100;
+            filterSlider.value = GrayScale;
+            console.log({ GrayScale });
+            filterValue.textContent = `${GrayScale} %`;
+
+        }
+        // appyFilter();
+    });
+});
+
+const updateFilter = () => {
+    filterValue.textContent = `${filterSlider.value} %`;
+
+    const selectedFilter = document.querySelector('.filter .active').id;
+
+    if (selectedFilter == filtros.BRIGHTNESS) {
+
+        Brightness = filterSlider.value;
+    } else if (selectedFilter == filtros.SATURATION) {
+
+        Saturation = filterSlider.value;
+    } else if (selectedFilter == filtros.INVERSION) {
+
+        Inversion = filterSlider.value;
+    } else {
+
+        GrayScale = filterSlider.value
+    };
+
+    appyFilter();
+};
+
+[...rotateOptions].map(option => {
+    option.addEventListener('click', () => {
+        const { address } = option.dataset;
+        if (address === direccion.LEFT) {
+            rotate -= 90;
+        } else if (address === direccion.RIGHT) {
+            rotate += 90;
+        } else if (address === direccion.HORIZONTAL) {
+            flipHorizontal = flipHorizontal === 1 ? -1 : 1;
+        } else {
+            flipVertical = flipVertical === 1 ? -1 : 1;
+        }
+        appyFilter();
+    });
+});
+
+const restFilter = () => {
+
+    Brightness = 100;
+    Saturation = 100;
+    Inversion = 0;
+    GrayScale = 0;
+
+    rotate = 0;
+    flipHorizontal = 1;
+    flipVertical = 1;
+
+    filterOptions[0].click();
+    appyFilter();
+};
+
+const saveImage = () => {
+
+    const editFilter = {
+        FILTER: `brightness(${Brightness}%) saturate(${Saturation}%) invert(${Inversion}%) grayscale(${GrayScale})`
+    };
+
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = previeImg.naturalWidth;
+    canvas.height = previeImg.naturalHeight;
+
+    ctx.filter = editFilter.FILTER;
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    if (rotate !== 0) {
+        ctx.rotate(rotate * Math.PI / 180);
+    };
+    ctx.scale(flipHorizontal, flipVertical);
+    ctx.drawImage(previeImg, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+
+    const link = document.createElement('a');
+    link.download = 'image.jpg';
+    link.href = canvas.toDataURL();
+    link.click();
+};
+
+fileInput.addEventListener('change', loadImagen);
+filterSlider.addEventListener('input', updateFilter);
+restFilterBtn.addEventListener('click', restFilter);
+saveImageBtn.addEventListener('click', saveImage);
+choosoBtn.addEventListener('click', () => fileInput.click());
+
